fc9a7f2bf73051e9c3cc868f0d29a51559d62c63
João Vitor de Almeida Medeiros <joao.medeiros@useall365.onmicrosoft.com>
2024-10-31 13:30:38 -0300

    Configuração do JWT

--- Prontuario.Api/Program.cs
+++ Prontuario.Api/Program.cs
@@ -1,30 +1,10 @@
-using Microsoft.EntityFrameworkCore;
-using Prontuario.Aplicacao.Mappings;
-using Prontuario.Aplicacao.Pacientes;
-using Prontuario.Dominio.Pacientes;
-using Prontuario.Repositorio.Contextos;
 using Prontuario.Infra;
-using Prontuario.Repositorio.UnitOfWorks;
-using Prontuario.Repositorio.RepPatterns;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<Contexto>(options =>
-    options.UseSqlServer(connectionString, 
-    x => x.MigrationsAssembly(typeof(Contexto).Assembly.FullName)));
-
-builder.Services.AddAutoMapper(typeof(EntitiesToDtoMappingProfile));
-
 builder.Services.AddControllers();
-
-builder.Services.AddScoped<IAplicPaciente, AplicPaciente>();
-builder.Services.AddScoped<IRep<Paciente>, Rep<Paciente>>();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
+builder.Services.AddInfraStructure(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 builder.Services.AddInfraStrucureSwagger();
 
 var app = builder.Build();
---
+++ Prontuario.Infra/DependecyInjection.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Prontuario.Aplicacao.Mappings;
+using Prontuario.Aplicacao.Pacientes;
+using Prontuario.Dominio.Pacientes;
+using Prontuario.Repositorio.Contextos;
+using Prontuario.Repositorio.RepPatterns;
+using Prontuario.Repositorio.UnitOfWorks;
+using System.Text;
+
+namespace Prontuario.Infra
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfraStructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<Contexto>(options =>
+                options.UseSqlServer(connectionString,
+                 x => x.MigrationsAssembly(typeof(Contexto).Assembly.FullName)));
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:SecretKey"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(configuration["Jwt:Audience"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            services.AddAutoMapper(typeof(EntitiesToDtoMappingProfile));
+
+
+            services.AddScoped<IAplicPaciente, AplicPaciente>();
+            services.AddScoped<IRep<Paciente>, Rep<Paciente>>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
