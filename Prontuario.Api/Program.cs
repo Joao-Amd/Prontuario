c0215d63a82062e5c89a583fe83839ea899e25e2
João Vitor de Almeida Medeiros <joao.medeiros@useall365.onmicrosoft.com>
2024-10-31 08:16:08 -0300

    Adcionado recebimento do token  de autenticação

--- Prontuario.Api/Program.cs
+++ Prontuario.Api/Program.cs
@@ -1,19 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using Prontuario.Repositorio.Contextos;
+using Prontuario.Infra;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<Contexto>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString, 
+    x => x.MigrationsAssembly(typeof(Contexto).Assembly.FullName)));
 
 builder.Services.AddControllers();
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddInfraStrucureSwagger();
 
 var app = builder.Build();
 
---
+++ Prontuario.Infra/DependecyInjectionSwagger.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Prontuario.Infra
+{
+    public static class DependecyInjectionSwagger
+    {
+        public static IServiceCollection AddInfraStrucureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(x =>
+            {
+                x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Description",
+                });
+
+                x.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
